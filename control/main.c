1835744dadbe6523b8485e81af9db251a75e0c02
potados99 <potados99@gmail.com>
2018-08-25 12:45:04 +0900

    Main done

--- control/main.c
+++ control/main.c
@@ -1,22 +1,32 @@
 #include "serial.h"
 
 int main(int argc, const char *argv[]) {
-	if (argc < ARGS_MAX - 1) {
-		fprintf(stderr, "Arguments not enough.\n");
-		fprintf(stderr, "Usage: $EXCUTABLE [section]\n");
+        if (argc < 2) {
+            fprintf(stderr, "Arguments not enough.\n");
+            fprintf(stderr, "Usage: $EXCUTABLE [arg1] [arg2].\n");
+            exit(1);
+        }
+
+	if (argc - 1 > COMMAND_MAX) {
+		fprintf(stderr, "Too many arguments. Max is 3.\n");
 		exit(1);
 	}
 
-	if (argc > ARGS_MAX) ERROR("Too many arguments.\n")
-
 	char cmdBuff[CMDBUFF_MAX] = {0,};
-	memset (cmdBuff, 0, CMDBUFF_MAX);
-
-	for (int i = 1; i < argc; ++ i) {
-		if (strlen(argv[i]) > 0)
-			strcat(cmdBuff, argv[i]);
-		strcat(cmdBuff
+	for (int i = 0; i < sizeof(cmdBuff); ++ i) {
+		cmdBuff[i] = 0x00;
 	}
 
-	if (! send_command(cmdBuff)) ERROR("Failed getting response.\n")
+	for (int i = 1; i < argc; ++ i) {
+		if (i > COMMAND_MAX) break;
+
+		strcat(cmdBuff, argv[i]);
+	        strcat(cmdBuff, " ");
+	}
+        strcat(cmdBuff, "\n");
+
+	bool success = send_command(cmdBuff);
+	if (!success) ERROR("Faild getting response.\n")
+	
+	return success;
 }
