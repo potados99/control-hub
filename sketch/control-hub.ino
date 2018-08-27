bcaadf40891ea60cdf5aa9b8d251bf6c2f404bda
potados99 <potados99@gmail.com>
2018-08-28 02:43:20 +0900

    Add support for status

--- sketch/control-hub.ino
+++ sketch/control-hub.ino
@@ -22,6 +22,8 @@
 #define INTERRUPT_CODE 27 /* esc */
 #define TERMINATE '\n'
 #define TERMINATE_OPTIONAL ';'
+#define PWM_VAL_RATE 2.55
+#define RAPID_DELAY 50
 
 #define BUZZER_CONTROL_PIN 2
 #define LIT_BUTTON_PIN 19
@@ -86,10 +88,10 @@ void serial_recieve_task() {
 
   if ((recieved == TERMINATE) || (recieved == TERMINATE_OPTIONAL)) {
     if (do_action(Input)) {
-      Serial.write("T\n");
+      // Serial.print("T\n");
     }
     else {
-      Serial.write("F\n");
+      Serial.print("F" + TERMINATE);
     }
     Input = "";
     return; /* once LF came, return. */
@@ -101,7 +103,7 @@ void serial_recieve_task() {
 void lit_button_input_task() {
   if (! digitalRead(LIT_BUTTON_PIN)) { /* Button is pushed */
     if ((~BtnSt & BTN_IS_PUSHED) && (~BtnSt & BTN_JUST_TOGGLED)) {
-      toggle(LIT_CONTROL_PIN);
+      _toggle(LIT_CONTROL_PIN);
     }
     BtnSt |= BTN_IS_PUSHED; /* Add 0000 0001 */
     BtnSt |= BTN_JUST_TOGGLED; /* Add 0000 0010 */
@@ -153,105 +155,63 @@ bool do_action(String incommingString) {
   for (int i = argStart; i < PARAM_MAX; ++ i) { commands[i] = split(incommingString, ' ', i); }
 
   if (commands[argStart] == "LIT") {
-    return power_control(LIT_CONTROL_PIN, commands, argStart + 1);
+    return device_control(LIT_CONTROL_PIN, NULL, commands, argStart + 1);
   }
   else if (commands[argStart] == "LED") {
-    return pwm_power_control(LED_CONTROL_PIN, &LedBrightness, commands, argStart + 1);
+    return device_control(LED_CONTROL_PIN, &LedBrightness, commands, argStart + 1);
   }
   else if (commands[argStart] == "FAN") {
-    return power_control(FAN_CONTROL_PIN, commands, argStart + 1);
+    return device_control(FAN_CONTROL_PIN, NULL, commands, argStart + 1);
   }
   else {
     return false;
   }
 }
 
-void beep(int howMany) {
-  BuzCnt += howMany;
-}
-
-bool toggle(unsigned short pin) {
-  if (read(pin)) {
-    return off(pin);
-  }
-  else {
-    return on(pin);
-  }
-}
-
-bool on(unsigned short pin) {
-  digitalWrite(pin, HIGH);
-  beep(1);
-  return (read(pin));
-}
-
-bool pwm_on(unsigned short pin, int *pwmValp) {
-  analogWrite(pin, *pwmValp);
-  return true;
-}
-
-bool off(unsigned short pin) {
-  digitalWrite(pin, LOW);
-  beep(1);
-  return (!read(pin));
-}
-
-bool pwm_off(unsigned short pin) {
-  analogWrite(pin, 0);
-  return true;
-}
-
-bool read(unsigned short pin) {
-  return (bool)bitRead(PORTD,pin);
-}
-
-bool power_control(unsigned short pin, String *args, int argStart) {
+bool device_control(unsigned short pin, int *pwmValp, String *args, int argStart) {
   if (args[argStart] == "") {
     beep(2);
     return false;
   }
 
   if (args[argStart] == "ON") {
-    return on(pin);
+    return power_control(pin, pwmValp, true);
   }
   else if (args[argStart] == "OFF") {
-    return off(pin);
+    return power_control(pin, pwmValp, false);
   }
   else if (args[argStart] == "RPD") {
-    return rapid_toggle(pin, args, argStart + 1);
+    return rapid_toggle(pin, pwmValp, args, argStart + 1);
   }
-  else if (args[argStart] == "BRT") {
-    return pwm_control(pin, args, argStart + 1);
+  else if (args[argStart] == "BRT") { // PWM only
+    return pwm_control(pin, pwmValp, args, argStart + 1);
   }
-  else if (args[argStart] == "SPD") {
-    return pwm_control(pin, args, argStart + 1);
+  else if (args[argStart] == "SPD") { // PWM only
+    return pwm_control(pin, pwmValp, args, argStart + 1);
+  }
+  else if (args[argStart] == "ST") {
+    return status_return(pin, pwmValp, args, argStart + 1);
   }
   else {
     return false;
   }
+
 }
 
-bool pwm_power_control(unsigned short pin, int *pwmValp, String *args, int argStart) {
-  if (args[argStart] == "") {
-    beep(2);
-    return false;
-  }
+bool power_control(unsigned short pin, int *pwmValp, bool power) {
+  bool hasPwmState = (pwmValp != NULL);
 
-  if (args[argStart] == "ON") {
-    return pwm_on(pin, pwmValp);
-  }
-  else if (args[argStart] == "OFF") {
-    return pwm_off(pin);
-  }
-  else if (args[argStart] == "BRT") {
-    return pwm_control(pin, pwmValp, args, argStart + 1);
-  }
-  else if (args[argStart] == "SPD") {
-    return pwm_control(pin, pwmValp, args, argStart + 1);
+  if (hasPwmState) {
+    analogWrite(pin, (power) ? *pwmValp : 0);
+    beep(1);
+    return true;
   }
   else {
-    return false;
+    digitalWrite(pin, power);
+    beep(1);
+    return (read(pin));
   }
+
 }
 
 bool pwm_control(unsigned short pin, int *pwmValp, String *args, int argStart) {
@@ -259,6 +219,7 @@ bool pwm_control(unsigned short pin, int *pwmValp, String *args, int argStart) {
     beep(2);
     return false;
   }
+  if (pwmValp == NULL) return false;
 
   int inputInt = args[argStart].toInt(); /* 0 to 100 val */
   if ((inputInt < 0) || (inputInt > 100)) {
@@ -266,60 +227,113 @@ bool pwm_control(unsigned short pin, int *pwmValp, String *args, int argStart) {
     return false;
   }
 
-  int pwmVal = inputInt * 2.55;
-  *pwmValp = pwmVal;
+  *pwmValp = inputInt;
 
-  analogWrite(pin, pwmVal);
+  analogWrite(pin, (*pwmValp) * PWM_VAL_RATE);
   return true;
 }
 
-bool pwm_control(unsigned short pin, String *args, int argStart) {
+bool rapid_toggle(unsigned short pin, int *pwmValp, String *args, int argStart) {
   if (args[argStart] == "") {
     beep(2);
     return false;
   }
 
-  int inputInt = args[argStart].toInt(); /* 0 to 100 val */
-  if ((inputInt < 0) || (inputInt > 100)) {
-    beep(2);
-    return false;
-  }
-
-  int pwmVal = inputInt * 2.55;
-
-  analogWrite(pin, pwmVal);
-  return true;
-}
-
-bool rapid_toggle(unsigned short pin, String *args, int argStart) {
-  if (args[argStart] == "") {
-    beep(2);
-    return false;
-  }
+  bool hasPwmState = (pwmValp != NULL);
 
   int duration = args[argStart].toDouble() * 1000;
   bool originState = read(pin);
 
   unsigned long startTime = millis();
 
-  for (;;) {
-    digitalWrite(pin, HIGH);
-    delay(10);
-    digitalWrite(pin, LOW);
-    delay(10);
+  if (hasPwmState) {
+    // PWM
+    for (;;) {
+      analogWrite(pin, *pwmValp);
+      delay(RAPID_DELAY);
+      analogWrite(pin, 0);
+      delay(RAPID_DELAY);
 
-    if (check_interrupt()) break;
-    if (millis() - startTime > duration) break;
+      if (check_interrupt()) break;
+      if (millis() - startTime > duration) break;
+    }
+
+    analogWrite(pin, (originState) ? *pwmValp : 0);
   }
+  else {
+    // Just on/off
+    for (;;) {
+      digitalWrite(pin, HIGH);
+      delay(RAPID_DELAY);
+      digitalWrite(pin, LOW);
+      delay(RAPID_DELAY);
 
-  digitalWrite(pin, originState);
+      if (check_interrupt()) break;
+      if (millis() - startTime > duration) break;
+    }
+
+    digitalWrite(pin, originState);
+  }
 
   return true;
 }
 
+bool status_return(unsigned short pin, int * pwmValp, String *args, int argStart) {
+  if (args[argStart] == "") {
+    beep(2);
+    return false;
+  }
+
+  if (args[argStart] == "PWR") {
+    return power_return(pin);
+  }
+  else if (args[argStart] == "BRT") {
+    return pwm_return(pwmValp);
+  }
+  else if (args[argStart] == "SPD") {
+    return pwm_return(pwmValp);
+  }
+  else {
+    return false;
+  }
+}
+
+bool power_return(unsigned short pin) {
+  String outString = read(pin) ? "ON" : "OFF";
+  Serial.print(outString + TERMINATE);
+  return true;
+}
+
+bool pwm_return(int *pwmValp) {
+  if (pwmValp == NULL) return false;
+
+  String outString = String(*pwmValp);
+  Serial.print(outString + TERMINATE);
+  return true;
+}
+
+bool read(unsigned short pin) {
+  return (bool)bitRead(PORTD,pin);
+}
+
+void beep(int howMany) {
+  BuzCnt += howMany;
+}
+
+
 bool check_interrupt() {
   if (! Serial.available()) return false;
   return Serial.read() == INTERRUPT_CODE;
+}
+
+
+bool _toggle(unsigned short pin) {
+  if (read(pin)) {
+    return off(pin);
+  }
+  else {
+    return on(pin);
+  }
 }
 #endif
 
