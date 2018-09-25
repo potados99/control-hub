9cee7f491a50d5e9cae23f8f087eca46c58b003a
potados99 <potados99@gmail.com>
2018-09-26 02:25:18 +0900

    Oh,,,,

--- arduino/libraries/PotadosProtocol/src/PotadosProtocol.cpp
+++ arduino/libraries/PotadosProtocol/src/PotadosProtocol.cpp
@@ -1,7 +1,91 @@
 #include "PotadosProtocol.h"
 
-PPManager::PPManager(uint8_t rx_pin, uint8_t tx_pin, uint8_t id)
+PPPacket::PPPacket(char type, uint8_t sender, uint8_t reciever, String data = "")
+{
+  this.type = type;
+  this.senderID = sender;
+  this.recieverID = reciever;
+  this.dataLength = data.length();
+  this.data = data;
+}
+
+String PPPacket::fillZeroHexString(uint8_t val, uint8_t digits = 2)
+{
+  String hexString = String(val, HEX);
+  String filledString;
+
+  for (uint8_t i = hexString.length(); i < digits; ++ i)
+  {
+    filledString += ' ';
+  }
+  filledString += hexString;
+
+  return filledString;
+}
+
+String PPPacket::toString()
+{
+  String stringOut;
+
+  stringOut += this.type;
+  stringOut += fillZeroHexString(this.senderID, 2);
+  stringOut += fillZeroHexString(this.recieverID, 2);
+  stringOut += fillZeroHexString(this.dataLength, 2);
+  stringOut += data;
+}
+
+
+PPSerial::PPSerial(uint8_t rx_pin, uint8_t tx_pin, uint8_t id)
 {
   this.serial = SoftwareSerial(rx_pin, tx_pin);
-  this.
+  this.id = id;
 }
+
+
+void PPSerial::send(char type, uint8_t dest, String data)
+{
+  PPPacket packet(type, this.id, dest, data);
+
+  this.serial.print(packet.toString() + TERMINATE);
+}
+
+void PPSerial::listen(callback feedbackBehavior)
+{
+  // Pass when not waiting for response.
+  if (~this.states & WAITING_FOR_RESPONSE) {
+    // not waiting for input. return.
+    return;
+  }
+  else if (millis() - this.lastSentTime > this.readTimeOut) {
+      // waiting but no response for 2 seconds. TIMEOUT!
+      this.states &= ~WAITING_FOR_RESPONSE;
+      this.states &= ~SUCCEEDED;
+      send("F");
+      beep(5);
+      return;
+  }
+
+  if (! this.serial.available()) return; /* nothing to do when nothing arrived. */
+
+  char recieved = this.serial.read();
+
+  if ((recieved == TERMINATE) || (recieved == TERMINATE_OPTIONAL)) {
+    if (verify(serial)) {
+      // if (Feedback) send("T");
+      feedbackBehavior("T");
+    }
+    else {
+      // if (Feedback) send("F");
+      feedbackBehavior("F");
+    }
+
+    this.inputBuffer = "";
+    // Feedback = true;
+
+    return; /* once LF came, return. */
+  }
+
+  this.inputBuffer += recieved;
+}
+
+bool
--- arduino/libraries/PotadosProtocol/src/PotadosProtocol.h
+++ arduino/libraries/PotadosProtocol/src/PotadosProtocol.h
@@ -4,44 +4,61 @@
 #include <Arduino.h>
 #include <SoftwareSerial.h>
 
+#define TERMINATE '\n'
+#define TERMINATE_OPTIONAL ';'
+
 typedef enum States {
   WAITING_FOR_RESPONSE = 0x01,
   SUCCEEDED = 0x02
-}
+} SERIAL_STATE;
 
-typedef enum Callback {
-  CALLBACK_NONE = 1,
-  CALLBACK_NEEDED
-};
+typedef enum Type {
+  ORDER = '!',
+  ASK = '?',
+  REPLY = ':',
+  REQUEST_RESEND = '~'
+} PACKET_TYPE;
 
-// serial wrapper class
-class SerialW
-{
-public:
-  SoftwareSerial*   port;
-  uint16_t          readTimeOut = 2000;
-  unsigned long     lastSentTime = 0;
-  unsigned long     lastRecievedTime = 0;
-  String            lastSentData; /* RF command sent through RF serial */
-  String            lastRecievedData;
-  String            inputBuffer; /* RF serial input */
-}
+typedef void (*callback)(String);
 
-class PPManager
+class PPPacket
 {
 private:
-  SerialW*          serial;
-  uint8_t           id;
-  uint8_t           states;
+  String            fillZeroHexString(uint8_t val, uint8_t digits = 2);
 
 public:
-  PPManager(uint8_t, uint8_t, uint8_t); // rx, tx, id
-  ~PPManager(void);
+  char              type;
+  uint8_t           senderID;
+  uint8_t           recieverID;
+  uint8_t           dataLength; // number of digits of hex data
 
-  void              begin(void);
-  void              end(void);
+  String            data;
 
-  void              send(String);
+  PPPacket(char type, uint8_t sender, uint8_t reciever, uint8_t dataLen, String data = "");
+
+  String            toString();
+}
+
+class PPSerial
+{
+private:
+  SoftwareSerial    serial;
+  uint8_t           id;
+  uint8_t           states = 0;
+  uint16_t          readTimeOut = 2000;
+  uint64_t          lastSentTime = 0;
+  uint64_t          lastRecievedTime = 0;
+  PPPacket          lastSentPacket = "";
+  PPPacket          lastRecievedPacket = "";
+  String            inputBuffer = "";
+
+  bool              parsePacket(String incommingString, PPPacket& packet);
+
+public:
+  PPSerial(uint8_t rx_pin, uint8_t tx_pin, uint8_t id); // rx, tx, id
+  ~PPSerial(void);
+
+  void              send(uint8_t dest, String data);
   void              listen(void);
   bool              verify(void);
 };
